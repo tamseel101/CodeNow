f22077ba0d57127d1974476cf5ea9daf616ab637
Abdullah Shahid <90593598+nxabdullah@users.noreply.github.com>
2023-02-05 23:04:37 -0500

    Change app name students to accounts & change project name semtrack_api to codenow_api

--- backend/codenow_api/urls.py
+++ backend/codenow_api/urls.py
@@ -1,4 +1,4 @@
-"""semtrack_api URL Configuration
+"""codenow_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from semtrack_api.students import views
+from codenow_api.accounts import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
