029f8c5dafefb42e38b2161f54c8972613e86dac
Abdullah Shahid <90593598+nxabdullah@users.noreply.github.com>
2023-02-06 15:53:00 -0500

    Convert login from function based view to a class based view

--- backend/codenow_api/accounts/views.py
+++ backend/codenow_api/accounts/views.py
@@ -1,20 +1,19 @@
+# django imports
 from django.contrib.auth.models import User
-from rest_framework import viewsets
-from rest_framework import permissions
-from rest_framework.response import Response
-from .serializers import UserSerializer
 from django.contrib.auth import authenticate
-from rest_framework.status import (
-    HTTP_400_BAD_REQUEST,
-    HTTP_404_NOT_FOUND,
-    HTTP_200_OK
-)
-from rest_framework.authtoken.models import Token
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.decorators import api_view, permission_classes
-from django.contrib.auth import authenticate
-from rest_framework.permissions import AllowAny
 
+# django rest imports
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.status import HTTP_400_BAD_REQUEST, \
+    HTTP_404_NOT_FOUND, HTTP_200_OK
+
+# local imports
+from .serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -24,23 +23,28 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
-@csrf_exempt
-@api_view(["post"])
-@permission_classes((AllowAny,))
-def login(request):
-    username = request.data.get("username")
-    password = request.data.get("password")
 
-    if username is None or password is None:
-        return Response({'error': 'Please provide both username and password'},
-                        status=HTTP_400_BAD_REQUEST)
+class LoginView(APIView):
+    permission_classes = [AllowAny]
 
-    user = authenticate(username=username, password=password)
+    @csrf_exempt
+    def post(self, request):
+        """
+        API endpoint that authenticates user and returns a token
+        """
+        username = request.data.get("username")
+        password = request.data.get("password")
 
-    if not user:
-        return Response({'error': 'Invalid Credentials'},
-                        status=HTTP_404_NOT_FOUND)
+        if username is None or password is None:
+            return Response({'error': 'Please provide both username and password'},
+                            status=HTTP_400_BAD_REQUEST)
 
-    token, _ = Token.objects.get_or_create(user=user)
-    return Response({'token': token.key},
-                    status=HTTP_200_OK)
+        user = authenticate(username=username, password=password)
+
+        if not user:
+            return Response({'error': 'Invalid Credentials'},
+                            status=HTTP_404_NOT_FOUND)
+
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key},
+                        status=HTTP_200_OK)
