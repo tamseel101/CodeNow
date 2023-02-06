806e78f76b262632f28c332d1357e737ce9b7370
Abdullah Shahid <90593598+nxabdullah@users.noreply.github.com>
2023-02-05 23:04:37 -0500

    Change app name students to accounts & change project name semtrack_api to codenow_api

--- backend/codenow_api/accounts/views.py
+++ backend/codenow_api/accounts/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
-from semtrack_api.students.serializers import UserSerializer, GroupSerializer
+from codenow_api.accounts.serializers import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
