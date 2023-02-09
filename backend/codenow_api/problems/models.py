ae08e807285659d8d548f77d40b7fd95aaa20572
Mohammad-Ali <mohammad@test-share.local>
2023-02-08 23:11:22 -0500

    implemented problem attempt functionality
    completed the following:
    - implemented problem attempt data model
    -preformed database migration to add problem_attempt table to database.
    - implemented post request end point to add new attempts to the database
    - implemented get request end point to retrieve problem attempts.

--- backend/codenow_api/problems/models.py
+++ backend/codenow_api/problems/models.py
@@ -4,3 +4,13 @@ from django.db import models
 class Categories(models.Model):
     name = models.CharField(max_length=300)
     description = models.CharField(max_length=1000)
+
+class Attempts(models.Model):
+    user_id = models.IntegerField()
+    problem_id = models.IntegerField()
+    category_id = models.IntegerField()
+    date = models.DateField()
+    perceived_difficulty = models.IntegerField()
+    time = models.IntegerField()
+    completed = models.BooleanField(default=False)
+
--- backend/codenow_api/problems/urls.py
+++ backend/codenow_api/problems/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CategoryView
+from .views import CategoryView, AttemptView
 
 urlpatterns = [
-    path('categories', CategoryView.as_view(), name='categories')
+    path('categories', CategoryView.as_view(), name='categories'),
+    path('attempts', AttemptView.as_view(), name='attempts')
 ]
