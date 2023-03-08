7210e169d6358724e712a8b3588961321a9e385a
kyle <krhobeck@gmail.com>
2023-03-08 16:11:01 -0500

    Updated file structure, added tests, created endpoints for the prequiz, added a LOT

--- backend/problems/views.py
+++ backend/problems/views.py
@@ -1,38 +1,38 @@
-from random import random
+# from random import random
 
-from rest_framework.views import APIView
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.response import Response
-from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
-from django.core import serializers as core_serializers
+# from rest_framework.views import APIView
+# from django.views.decorators.csrf import csrf_exempt
+# from rest_framework.response import Response
+# from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
+# from django.core import serializers as core_serializers
 #from .serializers import UserProblemsSerializer
-#from .models import UserProblems, PrequizProblem
-# from ..problems.models import Problem
+#from .models import problems, PrequizProblem
+# from ..prequiz.models import Problem
 """
 
 class UserProblemsView(APIView):
-    queryset = UserProblems.objects.all()
+    queryset = problems.objects.all()
     serializer_class = UserProblemsSerializer
 
     @csrf_exempt
     def get(self, request):
         ""
-        API endpoint that returns user problems
+        API endpoint that returns user prequiz
         ""
         # checking for the parameters from the URL
-        problems = UserProblems.objects.all()
-        count = len(problems)
-        problems_list = list(problems.values())
+        prequiz = problems.objects.all()
+        count = len(prequiz)
+        problems_list = list(prequiz.values())
         response = {
             'count': count,
-            'problems': problems_list[:]
+            'prequiz': problems_list[:]
         }
         return Response(response)
 
     @csrf_exempt
     def post(self, request):
         ""
-        API endpoint that will add a question to the UserProblems table.
+        API endpoint that will add a question to the problems table.
         ""
         question_id = request.data.get("question_id")
         user_duration = request.data.get("user_duration")
