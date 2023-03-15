49984b0570a60742a2387ef8034d9ed5e01cdfdb
Jenzeraku <59151550+Jenzeraku@users.noreply.github.com>
2023-03-15 19:17:27 -0400

    User story/behavioral page (#67)
    * moaidifed and added files to have create behavioral question page
    
    * Added static version of behaviour question cards and forms
    
    * modified layout of cards on behavioural page
    
    * Modified behavior question card to have a form
    
    * moaidifed and added files to have create behavioral question page
    
    * Create behavioral cards and form  (#60)
    
    * moaidifed and added files to have create behavioral question page
    
    * Added static version of behaviour question cards and forms
    
    * modified layout of cards on behavioural page
    
    * Modified behavior question card to have a form
    
    * modified files to support getting problems from endpoint
    
    * IMplement Behavioral Questions
    
    * behavioral page now works with backend
    
    ---------
    
    Co-authored-by: kyle <krhobeck@gmail.com>
    Co-authored-by: krhobeck <37283880+krhobeck@users.noreply.github.com>

--- backend/problems/views.py
+++ backend/problems/views.py
@@ -5,8 +5,8 @@
 # from rest_framework.response import Response
 # from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
 # from django.core import serializers as core_serializers
-#from .serializers import UserProblemsSerializer
-#from .models import problems, PrequizProblem
+# from .serializers import UserProblemsSerializer
+# from .models import problems, PrequizProblem
 # from ..prequiz.models import Problem
 """
 
@@ -46,3 +46,37 @@ class UserProblemsView(APIView):
         a.save()
         return Response(status=HTTP_200_OK)
 """
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from problems.models import BehaviorProblem
+from problems.serializers import BehavioralProblemsSerializer
+
+
+class BehavioralProblemsView(APIView):
+    """
+    Retrieves behavioral problems
+    """
+    serializer_class = BehavioralProblemsSerializer
+
+    def get(self, request, format=None):
+        problems = BehaviorProblem.objects.order_by('?').values('id', 'name', 'desc')[:9]
+        return Response({'problems': list(problems)})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get("name")
+            desc = serializer.validated_data.get("desc")
+            behavioral_question = BehaviorProblem.objects.create(
+                name=name,
+                desc=desc
+            )
+            behavioral_question.save()
+            return Response(status=status.HTTP_200_OK)
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
