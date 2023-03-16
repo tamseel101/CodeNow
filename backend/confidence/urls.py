d88e0b15208ab2e163f61715444df6046bb649b7
Mohammad-Ali <mohammad-ali@bandzar.com>
2023-03-16 00:55:39 -0400

    added code for confidence update endpoint
    developed confidence update url endpoint that will accept the results of the assessment survey and adjust the users confidence levels accordingly.

--- backend/confidence/urls.py
+++ backend/confidence/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('skill_assessment/', views.ConfidenceUpdateView.as_view(), name='skill_assessment'),
+    path('skill_assessment/', views.ConfidenceUpdateView.as_view(), name='skill_assessment'),
 ]
