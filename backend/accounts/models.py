f6642e512033a743c9967d929e2710d1bcf2a8de
Abdullah Shahid <90593598+nxabdullah@users.noreply.github.com>
2023-03-15 23:36:49 -0400

    Add confidence model and reference it in user

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 # Create your models here.
 class CustomUser(AbstractUser):
-    phone_number = models.CharField(max_length=20, null=True)
+    confidences = models.ManyToManyField('problems.ProblemCategory', through='confidence.Confidence', related_name='users')
