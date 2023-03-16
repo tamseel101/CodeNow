7631f0ec4c4086371f3bc63f1650dbb870b895d6
Abdullah Shahid <90593598+nxabdullah@users.noreply.github.com>
2023-03-15 21:50:55 -0400

    Remove prequiz dependency

--- backend/codenow_api/urls.py
+++ backend/codenow_api/urls.py
@@ -4,6 +4,5 @@ from rest_framework import routers
 
 urlpatterns = [
     path('account/', include('accounts.urls')),
-    path('prequiz/', include('prequiz.urls')),
     path('problems/', include('problems.urls')),
 ]
