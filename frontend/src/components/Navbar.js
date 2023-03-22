7f8c4861950bf1d25f764065ab2e525e24b6550a
Abdullah Shahid <90593598+nxabdullah@users.noreply.github.com>
2023-03-22 03:24:10 -0400

    Refactor the frontend by properly using the hooks, restructuring the entire directory structure, removing unused code and using better practices

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import useToken from "../hooks/useToken";
+import {Link} from "react-router-dom";
+
+const Navbar = () => {
+    const {token, removeToken} = useToken()
+
+    const Links = () => {
+        if (token) {
+            // Logged in links
+            return (
+                <>
+                    <Link to={'/dashboard'} className="nav-link active">Dashboard</Link>
+                    <Link to={'/skill-assessment'} className="nav-link active">Skill Assessment</Link>
+                    <Link to={'/behavioral'} className="nav-link active">Behavioral</Link>
+                    <Link to={'/'} className="nav-link active" onClick={() => removeToken()}>Logout</Link>
+                </>
+            )
+        } else {
+            // Logged out links
+            return (
+                <>
+                    <Link to={'/'} className="nav-link active">Home</Link>
+                    <Link to={'/about'} className="nav-link active">About</Link>
+                    <Link to={'/login'} className="nav-link active">Login</Link>
+                    <Link to={'/register'} className="nav-link active">Register</Link>
+                </>
+            )
+        }
+    }
+
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light ">
+                <div className="container">
+                    <a className="navbar-brand" href="/">CodeNext</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <Links />
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ frontend/src/components/leet_question/LeetQuestion.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './LeetQuestion.css';
+import {useNavigate} from "react-router-dom";
+
+function LeetQuestion(props) {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/Attempt', {state: {problem_id: props.problem_id, user_id: 1}});
+    };
+
+    return (
+        <Card className="mt-4 QuestionBackground">
+            <Card.Body>
+                <Row className="d-flex align-items-center">
+                    <Col sm={10}>
+                        <Card.Title className="text-light fw-bold"><h2>{props.name}</h2></Card.Title>
+                        <Card.Text className="DescText">{props.desc}</Card.Text>
+                    </Col>
+                    <Col sm={2}>
+                        <a target="_blank" href={props.url} rel="noreferrer">
+                            <Button
+                                className="QuestionButton"
+                                onClick={handleClick}>
+                                Code Next
+                            </Button>
+                        </a>
+                    </Col>
+                </Row>
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default LeetQuestion;
