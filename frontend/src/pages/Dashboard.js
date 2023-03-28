2010a4f33e38e7d36dc437b23d6a90aca839dd39
Abdullah Shahid <90593598+nxabdullah@users.noreply.github.com>
2023-03-28 04:25:21 -0400

    Force first-time users to complete the quiz

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,31 +1,50 @@
 import axios from "axios";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Container from 'react-bootstrap/Container';
+import { Link } from "react-router-dom";
 import Problems from "../components/Problems";
 import useToken from "../hooks/useToken";
 
-/* eslint-disable no-unused-vars */
-//let username = JSON.parse(sessionStorage.getItem("username"))
-
-
 export const Dashboard = () => {
-
     const {token} = useToken()
     axios.defaults.headers.common['Authorization'] = `Token ${token}`;
+    const [quizCompleted, setQuizCompleted] = useState(false);
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/confidence/skill_assessment/')
+            .then(response => {
+                const quizData = response.data;
+                if (Object.values(quizData).some(val => val === 0)) {
+                    setQuizCompleted(false);
+                } else {
+                    setQuizCompleted(true);
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
 
     return (
         <div>
             <Container className="pt-5 pb-5">
-                <h1 className="display-1">Hello!</h1>
-                <h3>Here are your recommended problems for today.</h3>
-                <p>Click on the Code Next button to start a problem.
-                    Then come back to this page to track your progress.</p>
-                    <Problems/>
+                {quizCompleted ? (
+                    <div>
+                        <h1 className="display-1">Hello!</h1>
+                        <h3>Here are your recommended problems for today.</h3>
+                        <p>Click on the Code Next button to start a problem.
+                            Then come back to this page to track your progress.</p>
+                        <Problems />
+                    </div>
+                ) : (
+                    <div>
+                        <h1 className="display-1">Welcome to CodeNext!</h1>
+                        <h3>To get started, you are required to complete the <Link className="text-success" to="/skill-assessment">skill assessment</Link> in order for us to provide appropriate recommendations.</h3>
+                    </div>
+                )}
             </Container>
         </div>
-
     );
 }
 
 export default Dashboard;
-/* eslint-enable no-unused-vars */
