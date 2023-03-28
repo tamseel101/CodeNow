b72b0c895907fdaa4ec9ecd4e9d20648184a7f3e
Abdullah Shahid <90593598+nxabdullah@users.noreply.github.com>
2023-03-28 03:04:00 -0400

    Use useEffect to prevent questions from reloading all the time

--- frontend/src/components/Problems.js
+++ frontend/src/components/Problems.js
@@ -1,38 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import LeetQuestion from "./leet_question/LeetQuestion";
-import {useQuery} from 'react-query';
-import axios from 'axios';
+import axios from "axios";
 
-axios.defaults.xsrfCookieName = 'csrftoken'
-axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeaderName = "X-CSRFToken";
 
 export const Problems = () => {
+  const [problems, setProblems] = useState([]);
+  const [status, setStatus] = useState("loading");
 
-    const {data: problems, status} = useQuery('prequiz', async () => {
-        const {data: problems} = await axios.get('http://localhost:8000/problems/recommended/');
-        return problems['results']
-    });
+  useEffect(() => {
+    const fetchProblems = async () => {
+      try {
+        const { data: problemsData } = await axios.get(
+          "http://localhost:8000/problems/recommended/"
+        );
+        setProblems(problemsData["results"]);
+        setStatus("success");
+      } catch (error) {
+        setStatus("error");
+      }
+    };
 
+    fetchProblems();
+  }, []);
 
-    return (
-        <div>
-            {status === 'loading' && <div>Loading...</div>}
-            {status === 'error' && <div>Error</div>}
-            {status === 'success' && (
-                <ul>
-                    {problems.map(problem => (
-                        <LeetQuestion
-                            name={problem.name}
-                            url={problem.leetcode_url}
-                            desc={problem.difficulty}
-                            problem_id={problem.id}
-                            key={problem.id}
-                        />
-                    ))}
-                </ul>
-            )}
-        </div>
-    );
-}
+  return (
+    <div>
+      {status === "loading" && <div>Loading...</div>}
+      {status === "error" && <div>Error</div>}
+      {status === "success" && (
+        <ul>
+          {problems.map((problem) => (
+            <LeetQuestion
+              name={problem.name}
+              url={problem.leetcode_url}
+              desc={problem.difficulty}
+              problem_id={problem.id}
+              key={problem.id}
+            />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default Problems;
